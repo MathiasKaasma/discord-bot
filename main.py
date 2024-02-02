a005ce029f5c1da5ab6c1165b266c5ff427d1a5b
MathiasKaasma <106476093+MathiasKaasma@users.noreply.github.com>
2024-02-02 11:40:36 +0200

    Init repo

---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+import responses
+
+
+async def send_message(message, user_message):
+    try:
+        response = responses.handle_response(user_message)
+        await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel)
+
+    print(f"{username} said: '{user_message}' ({channel})")
+
+    if user_message[0] == '!':
+        user_message = user_message[1:]
+        await send_message(message, user_message)
+
+
+client.run('LISA TOKEN')
---
+++ responses.py
@@ -0,0 +1,5 @@
+def handle_response(message) -> str:
+    p_message = message.lower()
+
+    if p_message == "abi":
+        return "vastus mille bot discordi saadab"
