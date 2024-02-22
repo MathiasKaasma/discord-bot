0d849b34983d1bfa1acfc28425ec02a6106707bc
MathiasKaasma <106476093+MathiasKaasma@users.noreply.github.com>
2024-02-22 11:59:51 +0200

    Added more comments

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord  # discord.py package
-import responses
+import responses  # Kõrvalfail, mida muutma hakkame
 
 
+# Sõnumi saatmine kasutajale
 async def send_message(message, user_message):
     try:
         response = responses.handle_response(user_message)
@@ -9,7 +10,7 @@ async def send_message(message, user_message):
     except Exception as e:
         print(e)
 
-
+# Boti registreerimine
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
@@ -17,11 +18,13 @@ intents.members = True
 client = discord.Client(intents=intents)
 
 
+# Tegevus, kui bot valmis laeb
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
 
+# Tegevus, kui keegi serveriga liitub
 @client.event
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.channels, name='general')
@@ -29,20 +32,26 @@ async def on_member_join(member):
         await channel.send(f"Welcome {member.mention} to the server!")
 
 
+# Tegevus, kui keegi serveris sõnumi saadab
 @client.event
 async def on_message(message):
+    # Ignoreerime boti enda sõnumeid
     if message.author == client.user:
         return
 
+    # Võtame sõnumi detailid muutujatesse
     username = str(message.author)
     user_message = str(message.content)
     channel = str(message.channel)
 
+    # Prindime enda jaoks PyCharmis iga saadetud sõnumi
     print(f"{username} said: '{user_message}' ({channel})")
 
+    # Kui sõnum eksisteerib ja algab !-ga, hakkame seda töötlema
     if user_message and user_message[0] == '!':
         user_message = user_message[1:]
         await send_message(message, user_message)
 
 
-client.run('TOKEN')
+# Paneme boti oma tokeniga tööle
+client.run('TOKEN')  # <- siinne token enda omaga ära muuta!
--- responses.py
+++ responses.py
@@ -1,5 +1,8 @@
+# Funktsioon, mis hakkab kasutajate sõnumeid käsitlema
 def handle_response(message) -> str:
+    # Teeme iga sõnumi väiketäheliseks, et endal oleks kergem
     p_message = message.lower()
 
+    # Kui sõnum oli !abi, saadame kasutajale vastuse
     if p_message == "abi":
-        return "vastus mille bot discordi saadab"
+        return "Vastus, mille bot discordi saadab"
